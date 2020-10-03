5da4a94f9524bc77ea120b9258da24778c97a56e
Lastvision <daniel.carlsson92@gmail.com>
2020-10-03 19:41:55 +0200

    Added skeleton enemy behaviour

---
+++ Assets/Scripts/NPC/EnemyBehaviour.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public enum STATE
+    {
+        IDLE,
+        AGGRO
+    }
+
+    protected STATE state = STATE.IDLE;
+    protected float stateTime = 0.0f;
+
+    void Start()
+    {
+        OnInit();
+    }
+    void FixedUpdate()
+    {
+        switch (state)
+        {
+            case STATE.IDLE:
+            {
+                OnUpdateIdleState();
+                break;
+            }
+            case STATE.AGGRO:
+            {
+                OnUpdateAggroState();
+                break;
+            }
+            default:
+            {
+                Debug.Log("Error: the state "+ state.ToString() + " is not a valid state");
+                break;
+            }
+        }
+    }
+
+    protected virtual void OnInit()
+    {
+
+    }
+    protected virtual void OnUpdateIdleState()
+    {
+
+    }
+
+    protected virtual void OnUpdateAggroState()
+    {
+
+    }
+
+    protected virtual void OnPreStateChanged(STATE newState, float newStateTime)
+    {
+
+    }
+
+    protected virtual void OnPostStateChanged(STATE newState, float newStateTime)
+    {
+
+    }
+
+    protected void ChangeState(STATE newState, float newStateTime)
+    {
+        if(state != newState)
+        {
+            OnPreStateChanged(newState, newStateTime);
+            state = newState;
+            stateTime = newStateTime;
+            OnPostStateChanged(newState, newStateTime);
+        }
+    }
+
+    protected float GetDistanceBetweenMeAndPlayer()
+    {
+        float distance = 0.0f;
+
+        Vector2 playerPos = GetPlayerCoordinate();
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+
+        distance = Vector2.Distance(pos, playerPos);
+
+        return distance;
+    }
+    protected Vector2 GetPlayerCoordinate()
+    {
+        Vector2 playerCoord = Vector2.zero;
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if(player)
+        {
+            playerCoord = new Vector2(player.transform.position.x, player.transform.position.z);
+        }
+
+        return playerCoord;
+    }
+}
---
+++ Assets/Scripts/NPC/KamikazeBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KamikazeBehaviour : EnemyBehaviour
+{
+    [SerializeField]
+    public float aggroRadius = 3.0f;
+    [SerializeField]
+    public float attackRadius = 1.0f;
+    [SerializeField]
+    public float explosionRadius = 2.0f;
+    [SerializeField]
+    public float explosionDamage = 1.0f;
+    [SerializeField]
+    public float movementSpeed = 10.0f;
+
+    private Vector2 velocity = Vector2.zero;
+    private Vector2 targetPoint = Vector2.zero;
+
+    protected override void OnInit()
+    {
+
+    }
+    protected override void OnUpdateIdleState()
+    {
+        float playerDist = GetDistanceBetweenMeAndPlayer();
+        if(playerDist < aggroRadius && stateTime - Time.fixedTime > 1.0f)
+        {
+            ChangeState(STATE.AGGRO, Time.fixedTime);
+            return;
+        }
+    }
+
+    protected override void OnUpdateAggroState()
+    {
+        float playerDist = GetDistanceBetweenMeAndPlayer();
+        if(playerDist > aggroRadius && stateTime - Time.fixedTime > 1.0f)
+        {
+            ChangeState(STATE.IDLE, Time.fixedTime);
+            return;
+        }
+        if(playerDist < attackRadius && stateTime - Time.fixedTime > 0.5f)
+        {
+            Explode();
+            return;
+        }
+    }
+
+    private void Explode()
+    {
+        print("Boom");
+    }
+}
