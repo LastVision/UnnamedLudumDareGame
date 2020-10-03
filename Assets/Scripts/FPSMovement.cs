88e1403b6075246ae88678d017eb90cfe6c52a14
Bearkowsky <bjornstenlund@outlook.com>
2020-10-03 12:23:39 +0200

    Add fps controlls and collisions to player prefab

--- Assets/Scripts/FPSMovement.cs
+++ Assets/Scripts/FPSMovement.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class FPSMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float minX = -60f;
+	public float maxX = 60f;
+	public float speed = 15f;
 
-    // Update is called once per frame
+	public float sensitivity;
+	public Camera cam;
+
+	float rotY = 0f;
+	float rotX = 0f;
+	void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
     void Update()
     {
-        
+		rotY += Input.GetAxis("Mouse X") * sensitivity;
+		rotX += Input.GetAxis("Mouse Y") * sensitivity;
+
+		rotX = Mathf.Clamp(rotX, minX, maxX);
+		transform.localEulerAngles = new Vector3(0, rotY, 0);
+		cam.transform.localEulerAngles = new Vector3(-rotX, rotY, 0);
+
+        var move = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")).normalized;
+        transform.Translate(move * speed * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+        	//Mistake happened here vvvv
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+
+		if (Cursor.visible && Input.GetMouseButtonDown(1))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
     }
+
 }
