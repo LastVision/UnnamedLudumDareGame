28d5a6d913282bb38d6adf1fa35e99c210cc3a27
Bananplyte <bananplyte@gmail.com>
2020-10-03 10:59:01 +0200

    Update InputManager.cs
    Added Restart button - Will later be used for reset

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -13,6 +14,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("R")) // RESTART - TEST FOR LATER RESET FUNCTION
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
