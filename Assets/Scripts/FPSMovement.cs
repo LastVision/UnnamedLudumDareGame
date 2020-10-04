386b4404a579cbd4be85927a4fcc148a078f43f4
Bananplyte <bananplyte@gmail.com>
2020-10-04 23:08:33 +0200

    Footsteps and landing!

--- Assets/Scripts/FPSMovement.cs
+++ Assets/Scripts/FPSMovement.cs
@@ -7,20 +7,31 @@ public class FPSMovement : MonoBehaviour
     //public
 	public Camera cam;
     public List<AudioClip> jumpAudioClips = new List<AudioClip>();
+    public List<AudioClip> landAudioClips = new List<AudioClip>();
+    public List<AudioClip> stepAudioClips = new List<AudioClip>();
 	public float speed = 10f;
     public float jumpForce = 1f;
 	public float sensitivity;
 
     //private
     private float sensitivityScale = 10f;
+    private float footstepTimer = 0.0f;
+    private float footstepCooldown = 0.3f;
     private bool isGrounded = true;
+    private AudioSource playerSource = null;
+    private int myLastPlayedStepIndex = 0;
+    private float distToGround = 0;
+    private float landCooldown = 0.4f;
+    private float landTimer = 0.0f;
 
 
     //Methods
 	void Start()
 	{
+        playerSource = GameObject.FindWithTag("Player").GetComponent<AudioSource>();
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
+        distToGround = gameObject.GetComponent<Collider>().bounds.extents.y;
 	}
 
     void FixedUpdate()
@@ -52,6 +63,34 @@ public class FPSMovement : MonoBehaviour
 			Cursor.lockState = CursorLockMode.Locked;
 			Cursor.visible = false;
 		}
+        if (isGrounded)
+        {
+            if (footstepTimer > 0.0f)
+            {
+                footstepTimer -= Time.deltaTime;
+            }
+            else
+            {
+                if (move.magnitude > 0.0f)
+                {
+                    int randomizedIndex = Random.Range(0, stepAudioClips.Count - 1);
+                    if (randomizedIndex == myLastPlayedStepIndex) // Make sure we don't get same audio twice
+                    {
+                        if (randomizedIndex > 0)
+                        {
+                            randomizedIndex--;
+                        }
+                        else
+                        {
+                            randomizedIndex++;
+                        }
+                    }
+                    playerSource.PlayOneShot(stepAudioClips[randomizedIndex]);
+                    myLastPlayedStepIndex = randomizedIndex;
+                    footstepTimer = footstepCooldown;
+                }
+            }
+        }
     }
 
     void Update()
@@ -60,13 +99,22 @@ public class FPSMovement : MonoBehaviour
         {
             GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
-            gameObject.GetComponent<AudioSource>().PlayOneShot(jumpAudioClips[Random.Range(0, jumpAudioClips.Count)]);
+            landTimer = landCooldown;
+            playerSource.PlayOneShot(jumpAudioClips[Random.Range(0, jumpAudioClips.Count)]);
         }
-        
+        if (!isGrounded && landTimer <= 0.0f)
+        {
+            if (Physics.Raycast(gameObject.transform.position, -Vector3.up, distToGround + 0.85f))
+            {
+                isGrounded = true;
+                playerSource.PlayOneShot(landAudioClips[Random.Range(0, landAudioClips.Count)]);
+            }
+        }
+        else if (landTimer > 0.0f)
+        {
+            landTimer -= Time.deltaTime;
+        }
+        Debug.Log(Physics.Raycast(gameObject.transform.position, -Vector3.up, distToGround + 0.85f));
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        isGrounded = true;;
-    }
 }
