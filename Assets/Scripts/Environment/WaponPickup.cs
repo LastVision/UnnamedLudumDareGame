c6ea636116b463585dcd3a174114d823c2fcf265
Bearkowsky <bjornstenlund@outlook.com>
2020-10-04 21:38:08 +0200

    Add pickups

--- Assets/Scripts/Environment/WaponPickup.cs
+++ Assets/Scripts/Environment/WaponPickup.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class WaponPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject PickupPrefab;
+    public float RotationsPerSecond = 1f;
+    private GameObject SpinningGunMesh;
+    private GameObject Light;
+    private bool IsPickedUp = false;
+
     void Start()
     {
-        
+        GameObject mesh = PickupPrefab.transform.Find("Mesh").gameObject;
+        SpinningGunMesh = Instantiate(mesh, transform.position + transform.up * 1.5f, Quaternion.identity);
+        Light = transform.gameObject.transform.Find("Light").gameObject;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!IsPickedUp)
+        {
+            SpinningGunMesh.transform.Rotate(Vector3.up * RotationsPerSecond * 360f * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player" && !IsPickedUp)
+        {
+            var weildingGun = collider.gameObject.GetComponent<WeildingGun>();
+            if (weildingGun)
+            {
+                PickupWeapon(weildingGun);
+            }
+        }
+    }
+
+    private void PickupWeapon(WeildingGun weildingGun)
+    {
+        weildingGun.EquipWeapon(PickupPrefab);
+        IsPickedUp = true;
+        Destroy(SpinningGunMesh);
+        Light.GetComponent<Light>().intensity = 0.2f;
     }
 }
