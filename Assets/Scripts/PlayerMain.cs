e6109b1e9127850e43a960521cbf89824488684d
Bearkowsky <bjornstenlund@outlook.com>
2020-10-04 23:31:32 +0200

    Player can die and it restart the level

---
+++ Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMain : MonoBehaviour
+{
+    public CameraFader CameraFader;
+
+    void Start()
+    {
+        CameraFader.FadeFromColor(Color.black, 1f);
+    }
+
+    void Kill()
+    {
+        CameraFader.FadeToColor(Color.black, 2.5f);
+        Invoke("ReloadLevel", 2.5f);
+        gameObject.GetComponent<FPSMovement>().enabled = false;
+        gameObject.GetComponent<WeildingGun>().enabled = false;
+    }
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene("Level");
+    }
+}
