57cb68a792088d0f471c0471228b975c3605e8ab
Lastvision <daniel.carlsson92@gmail.com>
2020-10-04 18:32:55 +0200

    Added EnviormentColor changer script

---
+++ Assets/Scripts/Enviorment/EnviormentColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EnviormentColor : MonoBehaviour
+{
+    [SerializeField]
+    public Color emissiveColor;
+
+    public bool setEmissiveColor = false;
+
+    void Update()
+    {
+       if(setEmissiveColor)
+       {
+           SetEmissiveColor();
+       }
+       setEmissiveColor = false;
+    }
+
+    private void SetEmissiveColor()
+    {
+        var renderers = GetComponentsInChildren<MeshRenderer>();
+        foreach(MeshRenderer renderer in renderers)
+        {
+            renderer.material.SetColor("_emissiveColor", emissiveColor);
+        }
+    }
+}
