fb296640013e98e222f49a40b759aede2f1e950f
Bearkowsky <bjornstenlund@outlook.com>
2020-10-04 22:40:31 +0200

    Added backseatgamer trigger when no gun

---
+++ Assets/Scripts/Environment/BackseatGamerEventTrigger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ECondition
+{
+    Pistol,
+    Shotgun,
+    RocketLauncher
+}
+
+public enum EBackseaterState
+{
+    HasFailed,
+    HasSucceeded,
+    Neither
+}
+
+public class BackseatGamerEventTrigger : MonoBehaviour
+{
+    public bool CostsStrikes = true;
+    public GameObject DoorToLock;
+    public ECondition Condition;
+    private bool HasMetCondition = false;
+    private EBackseaterState State = EBackseaterState.Neither;
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag != "Player")
+        {
+            return;
+        }
+
+        if (HasMetCondition)
+        {
+            Destroy(gameObject, 2);
+        }
+        else
+        {
+            if (DoorToLock)
+            {
+                DoorToLock.GetComponent<Door>().Lock();
+            }
+        }
+    }
+    public void ConditionMet(ECondition condition)
+    {
+        if (Condition == condition)
+        {
+            HasMetCondition = true;
+            DoorToLock.GetComponent<Door>().Unlock();
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/ConditionMetTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionMetTrigger : MonoBehaviour
+{
+    public ECondition TriggerConditionMet;
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag != "Player")
+        {
+            return;
+        }
+
+        var triggers = FindObjectsOfType<BackseatGamerEventTrigger>();
+        foreach (var t in triggers)
+        {
+            t.ConditionMet(TriggerConditionMet);
+        }
+
+    }
+}
